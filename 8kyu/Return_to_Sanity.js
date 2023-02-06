0007b712ddd0c1d372030cf59c204f1eded8aa5e
konstantos <sunnyday-23@mail.ru>
2023-02-06 17:19:00 +0300

    feat: add Return to Sanity

---
+++ 8kyu/Return_to_Sanity.js
@@ -0,0 +1,17 @@
+//TODO Задача: сделать так, чтобы функция возвращала объект
+//? Теория: Данный код нужно немнго подправить и убрать переносы строк. Всё.
+
+
+//* Что было:
+// function mystery() {
+//     var results =
+//       {sanity: 'Hello'};
+//     return
+//       results;
+//   }
+
+//* Что стало:
+function mystery() {
+    let results = {sanity: 'Hello'};
+    return results;
+}
