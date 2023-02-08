4a9ea7a5f0a369a6bcab93d42eedca6c8ebfdb8c
konstantos <sunnyday-23@mail.ru>
2023-02-08 18:38:56 +0300

    feat: add Grasshopper - Debug sayHello

---
+++ 8kyu/Grasshopper_Debug_sayHello.js
@@ -0,0 +1,6 @@
+//TODO Задача: Написать функцию, которая приветствует всех по определенному имени.
+//? Теория: Просто сделаем это через шаблонную строку. Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Template_literals
+
+
+//* Практика:
+const sayHello = name => `Hello, ${name}`;
