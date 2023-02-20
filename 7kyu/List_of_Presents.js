8e931d544ef3875d456193e6025e0345ca91f3ce
konstantos <sunnyday-23@mail.ru>
2023-02-20 15:38:03 +0300

    feat: fixed List of Presents

--- 7kyu/List_of_Presents.js
+++ 7kyu/List_of_Presents.js
@@ -1,10 +1,20 @@
+//TODO Задача: https://www.codewars.com/kata/5a84d485742ba347b90006b7/solutions/javascript
+//? Теория: лень писать
+
+
+/**
+ *  Функция возвращает максимальное количество покупок, которое можно совершить.
+ * @param {number} maxBudget Максимальная сумма для покупок
+ * @param {Array} gifts все товары, которые можно купить
+ * @returns {number} сколько товаров можно купить
+ */
 function howManyGifts(maxBudget, gifts) {
     gifts = gifts.sort((a, b) => a - b);
     let sum = 0;
     let i = 0;
     while (maxBudget >= sum) {
-      sum += gifts[i];
-      i++;
+        sum += gifts[i];
+        i++;
     }
     return i - 1;
-  }
+}
