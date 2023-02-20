c556823574e36d2f2075b71f8fc2a0dde90d1ce5
konstantos <sunnyday-23@mail.ru>
2023-02-20 14:21:35 +0300

    feat: add List of Presents

---
+++ 7kyu/List_of_Presents.js
@@ -0,0 +1,10 @@
+function howManyGifts(maxBudget, gifts) {
+    gifts = gifts.sort((a, b) => a - b);
+    let sum = 0;
+    let i = 0;
+    while (maxBudget >= sum) {
+      sum += gifts[i];
+      i++;
+    }
+    return i - 1;
+  }
