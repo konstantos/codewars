311c13c52b50fad32160b6354198f0336724b71e
konstantos <sunnyday-23@mail.ru>
2023-02-24 14:51:37 +0300

    feat: fix Switch Case - Bug Fixing #6

--- 8kyu/Switch_Case_-_Bug_Fixing_#6.js
+++ 8kyu/Switch_Case_-_Bug_Fixing_#6.js
@@ -1,3 +1,19 @@
+// function evalObject(value){
+//     var result = 0;
+//     switch(value.operation){
+//       case'+': result = value.a + value.b;
+//       case'-': result = value.a - value.b;
+//       case'/': result = value.a / value.b;
+//       case'*': result = value.a * value.b;
+//       case'%': result = value.a % value.b;
+//       case'^': result = Math.pow(value.a, value.b);
+//     }
+//     return result;
+//   }
+
+
+
+
 function evalObject(value){
     switch(value.operation) {
         case '+': return value.a + value.b;
