d38af3c810289faec83520737c33e35b27b6a2d4
konstantos <sunnyday-23@mail.ru>
2023-02-24 14:30:20 +0300

    feat: fix Calculate BMI

--- 8kyu/Calculate_BMI.js
+++ 8kyu/Calculate_BMI.js
@@ -10,3 +10,9 @@ function bmi(weight, height) {
         return "Obese";
     }
 }
+
+//* Упрощенная версия:
+function bmi(weight, height) {
+    let b = weight / height**2;
+    return b <= 18.5 ? 'Underweight' : b <= 25.0 ? 'Normal' : b <= 30.0 ? 'Overweight' : 'Obese';
+}
