bdfb6c2c7fe8bf884ec321700e7b688b8a7551b2
konstantos <sunnyday-23@mail.ru>
2023-02-24 14:23:53 +0300

    feat: add String Templates - Bug Fixing #5

---
+++ 8kyu/String_Templates_-_Bug_Fixing_#5.js
@@ -0,0 +1,9 @@
+// function buildString(...template){
+//     return `I like #{template.join(',')}!`;
+//   }
+
+
+
+function buildString(...template){
+    return `I like ${template.join(', ')}!`;
+}
