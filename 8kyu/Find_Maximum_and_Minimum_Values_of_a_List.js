45be3f6ddab78d4381a0ede157c59924f0bbdd22
konstantos <sunnyday-23@mail.ru>
2023-02-28 15:12:52 +0300

    feat: add Find Maximum and Minimum Values of a List

---
+++ 8kyu/Find_Maximum_and_Minimum_Values_of_a_List.js
@@ -0,0 +1,15 @@
+let min = function(list){
+    let m = list[0];
+      for (let i = 1; i < list.length; ++i) {
+        if (list[i] < m) m = list[i];
+      }
+    return m;
+  }
+
+  let max = function(list){
+      let m = list[0];
+      for (let i = 1; i < list.length; ++i) {
+        if (list[i] > m) m = list[i];
+      }
+    return m;
+  }
